a48a880daffcfb87d0345d3c39e562ce083ca9a6
lambricm <lambricm@mail.uc.edu>
2018-07-09 18:07:25 -0700

    can now access wide array of data

--- collect_data.py
+++ collect_data.py
@@ -2,22 +2,21 @@ from ao3.search import search
 from pathlib import Path
 import json
 import datetime
+from re import sub
 
 """
 	TODO:
-		- add work note retrieval to 'works' class
 		- retrieve chapter data
-			- also clear up any problems & add new data retrieval if needed in chapter class
 		- connect to db
-			- add data
+			- add data (choose sample fandom
 				- check data presence
 				- check data correctness
 """
 
 
 #fandom = "Ergo Proxy (Anime)"
-fandom = "Undertale (Video Game)"
-db_name = "ergo_proxy_pub"
+fandom = "Crimson Cross"
+db_name = "test_pub"
 timestamp_file = db_name + "_timestamp.txt"
 
 #retrieves list of works on same day/after prev. timestamp
@@ -29,6 +28,16 @@ def get_worklist(srch):
 	save_timestamp()
 	
 	return srch.get_result().get_all_works()
+	
+#retrieves list of works on same day/after prev. timestamp
+# srch -> the search you want to view the works from
+# return -> worklist item
+def get_work_iterator(srch):									
+	timestamp = format_timestamp(load_timestamp())
+	srch.set_date_from(timestamp)
+	save_timestamp()
+	
+	return srch.get_result().get_work_iterator()
 
 # prints all ids in worklist (used for testing)
 def print_work_ids(wrklst):
@@ -43,39 +52,131 @@ def print_work_ids(wrklst):
 	
 #CURRENTLY ONLY RETRIEVES DATA
 # wrklst -> retrieved worklist from search
-def add_work_data(wrklst):
+def add_work_data(wrkit):
+	print("Adding work data...")
 	try:
-		wrk = wrklst.next_work
+		wrk = wrkit.next_work()
 
 		while (not (wrk is None)):
-			id = wrk.id
+			
+			work_id = wrk.id
+			print("work id: " + str(work_id))
+			
 			fandoms = wrk.fandoms
+			for fandom in fandoms:
+				#check if exists in fandom table
+				#add if it doesn't
+				#get fandom id
+				#connect fandom id w/ work id
+				print("fandom: " + fandom)
+			
 			authors = wrk.author
+			#for author in authors:
+				#check if author exists in author table
+				#add if it doesn't
+				#get author id
+				#connect author id w/ work id
+			print("author: " + authors)
+			
 			tags = wrk.additional_tags
+			for tag in tags:
+				tag_search = search(tag).get_result()
+				parent_tag = tag_search.main_tag
+				#check if tag entry exists
+				#add tag & parent tag if doesn't exists
+				#update parent tag if needed
+				#get tag id
+				#connect tag & work
+				print("tag: " + tag)
+				print("parent tag: " + parent_tag)
+				
 			warnings = wrk.warnings
+			for warning in warnings:
+				#check exists
+				#add if no
+				#get id
+				#connect w/ fic
+				print("warning:" + warning)
+				
 			ratings = wrk.rating
+			for rating in ratings:
+				#check exists
+				#add if no
+				#get id
+				#connect w/ fic
+				print("rating:" + rating)
+				
 			relationships = wrk.relationship
+			for relationship in relationships:
+				#check exists
+				#add if no
+				#get id
+				#connect w/ fic
+				print("relationship:" + relationship)
+				
+			categories = wrk.category
+			for categories in categories:
+				#check exists
+				#add if no
+				#get id
+				#connect w/ fic
+				print("categories:" + categories)
+			
 			characters = wrk.characters
+			for character in characters:
+				#check exists
+				#add if no
+				#get id
+				#connect w/ fic
+				print("character:" + character)
+				
 			series = wrk.series
-			series_id = series["id"]
-			series = series["title"]
+			if not (series is None):
+				series_id = series["id"]
+				series = series["title"]
+				#check exists
+				#add if no
+				#connect w/ fic
+				print("series id, name: " + series_id + ", " + series)
+			
 			pub_date = wrk.published
+			print("publish date: " + str(pub_date))
+			
 			upd_date = wrk.updated
+			print("update date: " + str(upd_date))
+			
 			words = wrk.words
+			print("words: " + str(words))
+			
 			chapters = wrk.chapters
 			total_chapters = chapters["total_chapters"]
 			chapters = chapters["num_chapters"]
+			print("chapters: " + chapters + "/" + total_chapters)
+			
 			comments = wrk.comments
+			print("comments: " + str(comments))
+			
 			kudos = wrk.kudos
+			print("kudos: " + str(kudos))
+			
 			bookmarks = wrk.bookmarks
+			print("bookmarks: " + str(bookmarks))
+			
 			hits = wrk.hits
+			print("hits: " + str(hits))
+			
 			title = wrk.title
-			#note1
-			#note2
+			print("title: " + title)
+			
 			summary = wrk.summary
-						
-	except:
-		print("Error: no works in worklist")
+			summary = sub(r"<\/p><p>","\n",summary)
+			summary = sub(r"<\/p>|<p>","",summary)
+			print("summary: " + summary)
+
+			
+			wrk = wrkit.next_work()
+	except Exception as e:
+		print("ERROR: " + str(e))
 
 #ensures file exists
 # pth -> file to check
@@ -106,8 +207,8 @@ def save_timestamp():
 	curr_date = datetime.date.today()
 	timestamp = {"timestamp":{"year":curr_date.year, "month":curr_date.month, "day":curr_date.day}}
 	
-	with open(pth,'w') as outFile:
-		outFile.write(json.dumps(timestamp))
+	#with open(pth,'w') as outFile:
+	#	outFile.write(json.dumps(timestamp))
 
 #formats the timestamp for ao3 search compatability
 # timestamp -> loaded timestamp
@@ -133,4 +234,16 @@ def format_timestamp(timestamp):
 	return date_str
 			
 	
-res = get_worklist(search(fandom))
+	
+it = get_work_iterator(search(fandom))
+add_work_data(it)
+
+"""
+wrk = it.next_work()
+
+while not (wrk is None):
+	print(wrk.id)
+	wrk = it.next_work()
+	
+print(len(it.total_works))
+"""
